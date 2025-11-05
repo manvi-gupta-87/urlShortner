531c10689173a42ffe0e1d366caf0a1b78334438
manvi-gupta-87 <manvigupta1987@gmail.com>
2025-11-05 17:02:36 -0500

    Add navigation bar with logout functionality
    - Create navbar component with Material toolbar
    - Display app branding and logged-in username
    - Add dropdown menu with logout button
    - Show navbar only when user is authenticated
    - Integrate navbar into main app component

---
+++ frontend/url-shortener-ui/src/app/shared/components/navbar/navbar.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+  import { MatToolbarModule } from '@angular/material/toolbar';
+  import { MatButtonModule } from '@angular/material/button';
+  import { MatIconModule } from '@angular/material/icon';
+  import { MatMenuModule } from '@angular/material/menu';
+  import { AuthService } from '../../../core/services/auth.service';
+
+  @Component({
+    selector: 'app-navbar',
+    standalone: true,
+    imports: [
+      CommonModule,
+      MatToolbarModule,
+      MatButtonModule,
+      MatIconModule,
+      MatMenuModule
+    ],
+    templateUrl: './navbar.html',
+    styleUrls: ['./navbar.scss']
+  })
+
+  export class NavBar {
+    constructor(public authService : AuthService) {}
+
+    onLogout():void {
+        this.authService.logout();
+    }
+  } 
